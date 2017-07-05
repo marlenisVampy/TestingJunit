ed4184992e19e8a14631b582f4b90b4a168de8db
Marlenis Concepcion <vampy1277@gmail.com>
2017-07-05 15:03:14 -0600

    first commit

---
+++ Documents/NetBeansProjects/ComplexNumber/src/complexPackage/ComplexNumber.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package complexPackage;
+
+/**
+ *
+ * @author PC
+ */
+public class ComplexNumber {
+    
+private double Real;
+private double Imaginary;
+    public ComplexNumber(double re, double imag) {
+    Real = re;
+    Imaginary = imag;
+}
+    public double getReal() {
+    return Real;
+    }
+      public double getImaginary() {
+         return Imaginary;
+    }
+    public ComplexNumber add(ComplexNumber c) {
+        return new ComplexNumber(getReal() + c.getReal(),
+         getImaginary() + c.getImaginary());
+    }
+    public ComplexNumber multiply(ComplexNumber c) {
+    double re = getReal()*c.getReal() - getImaginary()*c.getImaginary();
+    double imag = getImaginary()*c.getReal() +
+    getReal()*c.getImaginary();
+    return new ComplexNumber(re, imag);
+    }
+    @Override
+    public boolean equals(Object anObject) {
+    if (anObject instanceof ComplexNumber) {
+        ComplexNumber c = (ComplexNumber) anObject;
+        return ((c.getReal() == getReal()) && (c.getImaginary() == getImaginary()));
+    } else
+    return false;
+    }
+    
+}
---
+++ Documents/NetBeansProjects/ComplexNumber/src/complexPackage/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package complexPackage;
+
+/**
+ *
+ * @author PC
+ */
+public class Main{
+public static void main(String[] args) {
+        // TODO code application logic here
+        
+        ComplexNumber newC = new ComplexNumber(4,5);
+        //newC.getImaginary();
+         System.out.println( newC.getImaginary());
+    }
+}
+    
