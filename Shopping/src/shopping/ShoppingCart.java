70ce753f486683c50028bdba7e079726aea61636
Marlenis Concepcion <vampy1277@gmail.com>
2017-07-10 00:09:24 -0600

    first commit

---
+++ Shopping/src/shopping/ShoppingCart.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shopping;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ *
+ * @author PC
+ */
+public class ShoppingCart {
+    
+    private ArrayList items;
+    
+    public ShoppingCart() {
+        items = new ArrayList();
+    }
+    
+    public double getBalance() {
+    double balance = 0.00;
+    for (Iterator i = items.iterator(); i.hasNext();)
+        {
+            Product item = (Product)i.next();
+            balance += item.getPrice();
+        }
+    return balance;
+        }
+    
+    public void addItem(Product item) {
+        items.add(item);
+    }
+    public void removeItem(Product item) {
+          items.remove(item);
+        }
+        public int getItemCount() {
+             return items.size();
+            }
+    public void empty() {
+        items.clear();
+        }
+}
